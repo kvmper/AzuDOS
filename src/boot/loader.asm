dc5d7bea92698d83f076982d55162e9d4841f7b6
kremure <kompuerr@gmail.com>
2026-09-12 21:11:12 +0000

    Protected mode support

--- src/boot/loader.asm
+++ src/boot/loader.asm
@@ -8,32 +8,72 @@ loader_start:
     ; Check if A20 is enabled
     call check_a20
     cmp ax, 1
-    je .after ; A20 is enabled yippiee
+    je .a20_enabled ; A20 is enabled yippiee
 .a20_disabled: ; A20 is disabled
     call bios_a20 ; Attempt enabling A20 line with BIOS
     ; Check if BIOS enabled the A20 Line
     call check_a20
     cmp ax, 1
-    je .after
+    je .a20_enabled
 
     call fast_a20 ; Attempt enabling A20 line with fast A20
     ; Check if fast A20 enabled the A20 line
     call check_a20
     cmp ax, 1
-    je .after
+    je .a20_enabled
     jmp error ; Failed to enable A20 line, error
-.after:
+.a20_enabled:
     mov [A20_ENABLED], 1
-    mov ah, 0x0E
-    mov al, 'A'
-    int 0x10
-    cli
-    hlt
+pm_setup:
+    ; Disable NMI
+    in al, 0x70
+    or al, 0x80
+    out 0x70, al
+    lgdt[gdt_desc]
+    ; Set protection enable bit in CR0
+    mov eax, cr0
+    or al, 1
+    mov cr0, eax
+
+    jmp 0x08:pm_main ; Jump to Protected Mode
+
+gdt:
+    dq 0x00000000 ; Null descriptor
+gdt_code_seg:
+    dw 0xFFFF ; Limit
+    dw 0x0000 ; Base (bits 0-15)
+    db 0x00 ; Base (bits 16-23)
+    db 10011010b ; Access byte
+    db 11001111b ; Flags
+    db 0x00 ; Base (bits 24-31)
+gdt_data_seg:
+    dw 0xFFFF ; Limit
+    dw 0x0000 ; Base (bits 0-15)
+    db 0x00 ; Base (bits 16-23)
+    db 10010010b ; Access byte
+    db 11001111b ; Flags
+    db 0x00 ; Base (bits 24-31)
+gdt_end:
+
+gdt_desc:
+    dw (gdt_end - gdt - 1) ; GDT limit
+    dd gdt ; GDT base
 
 error:
     mov ah, 0x0E
     mov al, 'E'
     int 0x10
+    hlt
+
+BITS 32 ; Now we're in Protected Mode
+pm_main:
+    mov eax, 0x10
+    mov ds, eax
+    mov es, eax
+    mov fs, eax
+    mov gs, eax
+
+    cli
     hlt
 
 BOOT_DEVICE db 0
